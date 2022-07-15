35a7471d0ad45a8791cb13529e55209a00e4f243
Frank Feng <frankfeng1998@gmail.com>
2022-07-14 21:05:48 -0500

    Add popup button that allows re-size

--- source.js
+++ source.js
@@ -5,23 +5,47 @@ function initialize(size){
     container.style.setProperty('--grid-cols', size);
     for (let c = 0; c < (size * size); c++) {
         let cell = document.createElement("div");
-        cell.textContent = (c + 1);
         container.appendChild(cell).className = "grid-item";
     };
 }
 
-initialize(16);
-
-function color(e){
-    this.classList.add('.color');
+function color(cells){
+    cells.forEach(cell => {
+        cell.addEventListener('mouseover', () => {
+            cell.classList.add('color');
+        });
+    });
 }
 
-const cells = document.querySelectorAll('.grid-item');
-cells.forEach(cell => {
-    cell.addEventListener('mouseover', () => {
-        cell.classList.add('color');
-    });
-});
+initialize(16);
 
+let cells = document.querySelectorAll('.grid-item');
+color(cells);
+
+function restart(){
+    let size = prompt("What sizes do you want the grid to be?");
+    size = Number(size);
+    if (size > 100){
+        alert("Sorry, the size is too big. Please select a number smaller than 100.")
+        let size = prompt("What sizes do you want the grid to be?");
+    }
+    remove(cells);
+    initialize(size);
+    cells = document.querySelectorAll('.grid-item');
+    color(cells);
+}
+
+function remove(cells){
+    let num = 0;
+    cells.forEach(cell => {
+        num++;
+        console.log(cell);
+        container.removeChild(cell);
+    })
+    console.log(`removed ${num} cells`);
+}
+
+const popup = document.querySelector('#popup');
+popup.addEventListener('click', restart);
 
 
